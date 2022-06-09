4a0a2f02fcd36c7d9ef4b8c0282eae5447b51364
Patrick Shriwise <pshriwise@gmail.com>
2022-06-08 23:02:41 -0500

    Some updates to the cml script

--- groups_to_blocks.py
+++ groups_to_blocks.py
@@ -1,51 +1,76 @@
 ### Command line version ###
-
 import sys
 import argparse
+from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Input for Cubit conversion script.')
-# IMP naming could be better, '--' may denote optional
-parser.add_argument("-g", "--geometry_path", type=str, help='Path to the Abaqus input file', required=True)
-parser.add_argument("-p", "--cubit_path", help="Path to Cubit executable (required if PATH does not include Cubit)", required=False)
-parser.add_argument("-o", "--output", help="Name/Location of the output file (Default is current/directory/exodus_out.exo)", required=False, nargs="exodus_out.exo")
+parser.add_argument("abaqus_file", type=Path, help='Path to the Abaqus input file')
+parser.add_argument("-p", "--cubit_path", type=Path, help="Path to Cubit executable (required if PATH does not include Cubit)", required=False)
+parser.add_argument("-o", "--output", type=Path, help="Name/Location of the output file (Default is current/directory/exodus_out.exo)", required=False, default="exodus_out.exo")
+parser.add_argument("-f", "--force", action='store_true', default=False,
+                    help="If present, overwrite a pre-existing output files.")
 
 # Should either add cubit to path beforehand or require input for the line below
 # IMP Could add try/catch
 # IMP could try to avoid importing sys
-
 args = parser.parse_args()
 
 # Makes sure cubit is importable
 if args.cubit_path is not None:
     sys.path.append(str(args.cubit_path))
 
-# Set up other input variables
-geom_path = str(args.geometry_path)
-output_path = str(args.geometry_path)
-# This may not be necessary, TODO test whether export needs full path if it does os.cwd may be used
-if output_path == "exodus_out.exo":
-    pwd = True
+# setup output command
+output_path = str(args.output)
+
+file_ext = str(args.output).split('.')[-1]
+
+if file_ext == ".cub":
+    output_cmd = 'save as "{}"'.format(output_path)
+elif file_ext in ('e', 'exo'):
+    output_cmd = 'export mesh "{}"'.format(output_path)
+else:
+    raise ValueError('Output extension "{}" not supported'.format(file_ext))
+
+# check for pre-existing output file
+if args.force:
+    output_cmd += ' overwrite'
+else:
+    if Path(output_path).exists:
+        msg = ('The output file "{}" already exists. Use "-f" '
+              'to overwrite output files.'.format(output_path))
+        raise RuntimeError(msg)
 
 ### Cubit portion of the code ###
 import cubit
-cubit.init()
+
+cubit.init([])
 cubit.cmd("reset")
 
 # Need to specify geom path for this command
-cubit.cmd("import abaqus mesh geometry  \"{}\" feature_angle 135.00".format(geom_path))
+cubit.cmd("import abaqus mesh geometry  \"{}\" feature_angle 135.00".format(args.abaqus_file))
 
-entityIDList = cubit.get_entities("group")
 cubit.cmd("merge vol all")
 cubit.cmd("block 1 remove vol all")
 
+# remove all pre-existing blocks
+block_ids = cubit.get_entities("block")
+for block in block_ids:
+    cubit.cmd("delete block {}".format(block))
+
 # This overwrites the default "picked" group which is probably cleaner and also
 # appends _<id> onto the end which avoids keywords
-for i in range(2,len(entityIDList)):
-    tempString = "block {} add group {}".format(i-1, i)
+split_key = '.SET-MATERIAL'
+
+group_ids = cubit.get_entities("group")
+for i, group in enumerate(group_ids):
+    tempName = cubit.get_entity_name("group", group)
+    # skip the picked group if present
+    if tempName == 'picked':
+        continue
+    tempString = "block {} add group {}".format(i, group)
     cubit.cmd(tempString)
-    tempName = cubit.get_entity_name("group", i)
-    blockName = "\"" + tempName.split('.')[0] + "_{}\"".format(i-1)
-    tempString = "block {} name {}".format(i-1,blockName)
+    blockName = "\"" + tempName.split(split_key)[0] + "_{}\"".format(i)
+    tempString = "block {} name {}".format(i,blockName)
     cubit.cmd(tempString)
 
-cubit.cmd("export mesh \"{}\"  overwrite".format(output_path))
+cubit.cmd(output_cmd)
